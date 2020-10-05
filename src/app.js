9bef915937473fc92336ac43d73285178f07421c
Edson Pérez Demanet <eds.perezd@gmail.com>
2020-10-05 16:54:36 -0300

    add encode & decode

--- src/app.js
+++ src/app.js
@@ -1,8 +1,51 @@
-require('dotenv').config();
+const crypto = require('crypto');
+const fs = require('fs');
+const logger = require('./utils/logger');
 
-const decode = () => {};
+const decode = (folder, algorithm, password) => {
+  logger.debug(`Folder ${folder}`);
+  logger.debug(`Algorithm ${algorithm}`);
+  logger.debug(`Password ${password}`);
 
-const encode = () => {};
+  const key = crypto.scryptSync(password, 'salt', 24);
+
+  logger.debug(`Key ${key.toString('hex')}`);
+
+  const iv = Buffer.alloc(16, 0);
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  const input = fs.createReadStream(`${folder}.enc`);
+  const output = fs.createWriteStream(`${folder}.dec.txt`, 'utf8');
+  input.pipe(decipher).pipe(output);
+  return new Promise((resolve, reject) => {
+    input.on('end', () => resolve());
+    input.on('error', (error) => reject(error));
+  });
+};
+
+const encode = (folder, algorithm, password) => {
+  logger.debug(`Folder ${folder}`);
+  logger.debug(`Algorithm ${algorithm}`);
+  logger.debug(`Password ${password}`);
+
+  const key = crypto.scryptSync(password, 'salt', 24);
+  const iv = Buffer.alloc(16, 0);
+
+  logger.debug(`Key ${key.toString('hex')}`);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  const input = fs.createReadStream(folder);
+  const output = fs.createWriteStream(`${folder}.enc`);
+
+  input.pipe(cipher).pipe(output);
+
+  return new Promise((resolve, reject) => {
+    input.on('end', () => resolve());
+    input.on('error', (error) => reject(error));
+  });
+};
 
 module.exports = {
   decode,
--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const { encode, decode } = require('./app');
 const logger = require('./utils/logger');
+
+const FOLDER = 'test.txt';
+const ALGORITHM = 'aes-192-cbc';
+const PASSWORD = 'qweqwe';
 
 logger.info(
   `Starting in ${process.env.NODE_ENV} mode and logger ${process.env.LOGGER_LEVEL} mode`
 );
+
+const init = async () => {
+  await encode(FOLDER, ALGORITHM, PASSWORD);
+  logger.info(`Encode done `);
+  await decode(FOLDER, ALGORITHM, PASSWORD);
+  logger.info(`Decode done`);
+};
+
+init();
